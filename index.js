545078bd767d66780345113c994169fca93304a9
skdv <felipe.osrn@gmail.com>
2022-06-11 17:17:29 -0500

    chore: privacy policy release on express server

---
+++ index.js
@@ -0,0 +1,20 @@
+import path from 'path'
+import parser from 'body-parser'
+import express from 'express'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+
+const app = express()
+const port = process.env.PORT || 8585
+
+app.use(parser.urlencoded({ extended: true }))
+app.use(parser.json())
+
+app.get('/', (_, res) => {
+    res.sendFile(path.join(__dirname, '/privacy-policy.html'))
+})
+
+app.listen(port)
+console.log('Servidor escuchando en puerto: ' + port)
